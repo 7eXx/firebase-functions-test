455fb1c662a0e6028b6f708a436d79b543e93cb7
marco.tessari <marco.tessari@fv.lan>
2025-02-09 16:33:34 +0100

    apply lint fix some rules for eslint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,12 +7,12 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
-import {onDocumentCreated} from "firebase-functions/v2/firestore";
+import { onDocumentCreated } from "firebase-functions/v2/firestore";
 // The Firebase Admin SDK to access Firestore.
-import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
 initializeApp();
 
@@ -24,38 +24,38 @@ initializeApp();
 //   response.send("Hello from Firebase!");
 // });
 
-export const addmessage = onRequest( async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Firestore using the Firebase Admin SDK.
-    const writeResult = await getFirestore()
-        .collection("messages")
-        .add({original: original});
-    // Send back a message that we've successfully written the message
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
+export const addmessage = onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await getFirestore()
+    .collection("messages")
+    .add({ original: original });
+  // Send back a message that we've successfully written the message
+  res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
 
 // Listens for new messages added to /messages/:documentId/original
 // and saves an uppercased version of the message
 // to /messages/:documentId/uppercase
 export const makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
-    // Verifica se event.data è definito
-    if (!event.data) {
-        logger.error("event.data è undefined");
-        return null;
-    }
+  // Verifica se event.data è definito
+  if (!event.data) {
+    logger.error("event.data è undefined");
+    return null;
+  }
 
-    // Grab the current value of what was written to Firestore.
-    const original = event.data.data().original;
+  // Grab the current value of what was written to Firestore.
+  const original = event.data.data().original;
 
-    // Access the parameter `{documentId}` with `event.params`
-    logger.log("Uppercasing", event.params.documentId, original);
+  // Access the parameter `{documentId}` with `event.params`
+  logger.log("Uppercasing", event.params.documentId, original);
 
-    const uppercase = original.toUpperCase();
+  const uppercase = original.toUpperCase();
 
-    // You must return a Promise when performing
-    // asynchronous tasks inside a function
-    // such as writing to Firestore.
-    // Setting an 'uppercase' field in Firestore document returns a Promise.
-    return event.data.ref.set({uppercase}, {merge: true});
+  // You must return a Promise when performing
+  // asynchronous tasks inside a function
+  // such as writing to Firestore.
+  // Setting an 'uppercase' field in Firestore document returns a Promise.
+  return event.data.ref.set({ uppercase }, { merge: true });
 });
